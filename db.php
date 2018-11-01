205e81f9c1679ed3eae59dcd9ce814a456fd3fea
bittokazi <bitto.kazi@gmail.com>
2018-11-02 00:12:28 +0600

    Heroku Deplyment

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
-	$DB_NAME = 'messanger';
-	$DB_HOST = '127.0.0.1';
-	$DB_USER = 'root';
-	$DB_PASS = '';
+	$DB_NAME = $_ENV["MYSQL_DB"];
+	$DB_HOST = $_ENV["MYSQL_HOST"].':'.$_ENV["MYSQL_PORT"];
+	$DB_USER = $_ENV["MYSQL_UN"];
+	$DB_PASS = $_ENV["MYSQL_PW"];
 	
 	global $mysqli;
 	$mysqli= mysqli_connect($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
